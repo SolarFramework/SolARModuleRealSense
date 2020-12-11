5ce2af6859e0ec52019e0901b5d2ee24ef387730
Jean-Marie HENAFF <Jean-Marie.HENAFF@b-com.com>
2020-12-11 11:54:56 +0100

    feat: remove 'using namespace' directives from headers

--- interfaces/SolARRGBDCameraRealSense.h
+++ interfaces/SolARRGBDCameraRealSense.h
@@ -25,7 +25,6 @@
 #include <librealsense2/rs.hpp>
 
 namespace SolAR {
-using namespace datastructure;
 namespace MODULES {
 namespace REALSENSE {
 
@@ -47,27 +46,27 @@ public:
     /// @param colorImg Will always be null
     /// @see getNextRGBDFrame()
     /// @return FrameworkReturnCode Return fail event
-    FrameworkReturnCode getNextImage(SRef<Image> & colorImg) override;
+    FrameworkReturnCode getNextImage(SRef<datastructure::Image> & colorImg) override;
 
     /// @brief At the moment we are not able to deliver you only the Depth Image, as we aquiere both depth and image at the same time
     /// Please use getNextRGBDFrame() instead.
     /// @param depthImg Will always be null
     /// @see getNextRGBDFrame()
     /// @return FrameworkReturnCode Return fail event
-    FrameworkReturnCode getNextDepthFrame(SRef<Image>& depthImg) override;
+    FrameworkReturnCode getNextDepthFrame(SRef<datastructure::Image>& depthImg) override;
 
     /// @brief Provides the last color image, depth image, corresponding 3D point cloud, and aligned images (RGB on depth and depth on RGB).
     /// If output parameters are null (nullptr), it means that the implementation, or the requested mode does not provide this feature.
     /// @param colorImg the RGB image captured by the RGBD camera
     /// @param depthImg the depth image captured by the RGBD camera
     /// @return FrameworkReturnCode to track sucessful or failing event.
-    FrameworkReturnCode getNextRGBDFrame(SRef<Image>& colorImg, SRef<Image>& depthImg) override;
+    FrameworkReturnCode getNextRGBDFrame(SRef<datastructure::Image>& colorImg, SRef<datastructure::Image>& depthImg) override;
 
     /// @brief Provides the corresponding 3D point cloud to last depth image aquiered (getNextRGBDFrame())
     /// Should have no effect if the user didn't call getNextRGBDFrame() beforehand
     /// @param pc the 3D point cloud reconstructed from the depth image. Points coordinates are defined according to the RGBD camera coordinate system.
     /// @return FrameworkReturnCode to track sucessful or failing event.
-    FrameworkReturnCode getPointCloud(SRef<PointCloud>& pc) override;
+    FrameworkReturnCode getPointCloud(SRef<datastructure::PointCloud>& pc) override;
 
 
     /// @brief Start the acquisition device reference
@@ -80,71 +79,71 @@ public:
 
     /// @brief Provides a depth image aligned on the color image
     /// @param alignedDepthImg the depth image captured by the RGBD camera and aligned on the color image
-    virtual FrameworkReturnCode alignDepthToColor (SRef<Image>& alignedDepthImg) const override;
+    virtual FrameworkReturnCode alignDepthToColor (SRef<datastructure::Image>& alignedDepthImg) const override;
 
     /// @brief Provides a color image alingned on the depth image
     /// @param alignedColorImg the RGB image captured by the RGBD camera and aligned on the depth image
-    virtual FrameworkReturnCode alignColorToDepth (SRef<Image>& alignedColorImg) const override;
+    virtual FrameworkReturnCode alignColorToDepth (SRef<datastructure::Image>& alignedColorImg) const override;
 
     /// @brief Provides the 3D point in the depth sensor coordinate system corresponding to a given pixel of the color image
     /// @param inPixel The pixel for which we want the 3 position
     /// @return a 3D point corresponding to the input pixel
-    virtual Point3Df getPixelToWorld (const Point2Di& inPixel) const override;
+    virtual datastructure::Point3Df getPixelToWorld (const datastructure::Point2Di& inPixel) const override;
 
     /// @brief Provides the pixel of the color image to the projection of a given 3D point
     /// @param in3DPoint The 3Dpoint we want to project on the color image
     /// @return a 2D point representing a pixel of the color image on which the 3D point in projected
-    virtual Point2Di getWorldToPixel (const CloudPoint& in3DPoint) const override;
+    virtual datastructure::Point2Di getWorldToPixel (const datastructure::CloudPoint& in3DPoint) const override;
 
     /// @brief Provides the pixels of the color image to the projection of given 3D points
     /// @param in3DPoints The 3D points we want to project on the color image
     /// @return a 2D points vector representing the pixels of the color image on which the 3D points are projected
-    virtual std::vector<Point2Df> getWorldToPixels (const std::vector<CloudPoint>& in3DPoints) const override;
+    virtual std::vector<datastructure::Point2Df> getWorldToPixels (const std::vector<datastructure::CloudPoint>& in3DPoints) const override;
 
     /// @brief Set the color image resolution of the acquisition device
-	void setResolution(const Sizei & resolution) override;
+	void setResolution(const datastructure::Sizei & resolution) override;
 
     /// @brief Set the depth image resolution of the acquisition device
-	FrameworkReturnCode setDepthResolution(Sizei resolution) override;
+	FrameworkReturnCode setDepthResolution(datastructure::Sizei resolution) override;
 
     /// @brief Set the intrinsic RGB camera parameters
-	void setIntrinsicParameters(const CamCalibration & intrinsic_parameters) override;
+	void setIntrinsicParameters(const datastructure::CamCalibration & intrinsic_parameters) override;
    
 	/// @brief Set the intrinsic parameters of the depth camera
-	FrameworkReturnCode setIntrinsicDepthParameters(const CamCalibration & intrinsic_parameters) override ;
+	FrameworkReturnCode setIntrinsicDepthParameters(const datastructure::CamCalibration & intrinsic_parameters) override ;
 
     /// @brief Set the distortion intrinsic parameters of the RGB camera
-	void setDistortionParameters(const CamDistortion & distortion_parameters) override ;
+	void setDistortionParameters(const datastructure::CamDistortion & distortion_parameters) override ;
 
     /// @brief Set the distortion intrinsic parameters of the depth camera
-	FrameworkReturnCode setDistortionDepthParameters(const CamDistortion & distortion_parameters) override ;
+	FrameworkReturnCode setDistortionDepthParameters(const datastructure::CamDistortion & distortion_parameters) override ;
 
 	/// @brief Set the distortion intrinsic camera parameters
-	void setParameters(const CameraParameters & parameters) override;
+	void setParameters(const datastructure::CameraParameters & parameters) override;
 
     /// @brief Get the image resolution of the RGB acquisition device
-    Sizei getResolution() override ;
+	datastructure::Sizei getResolution() override ;
 
     /// @brief Get the image resolution of the depth acquisition device
-    Sizei getDepthResolution() override ;
+	datastructure::Sizei getDepthResolution() override ;
 
 	/// @brief Get the min acquisition distance of the device
 	float getDepthMinDistance() override;
 
     /// @return Return the intrinsic RGB camera parameters
-	const CamCalibration & getIntrinsicsParameters() override ;
+	const datastructure::CamCalibration & getIntrinsicsParameters() override ;
 
 	/// @return Return the camera parameters
-	const CameraParameters & getParameters() override;
+	const datastructure::CameraParameters & getParameters() override;
 
     /// @return Return the intrinsic depth camera parameters
-    const CamCalibration& getIntrinsicsDepthParameters() const override ;
+    const datastructure::CamCalibration& getIntrinsicsDepthParameters() const override ;
 
     /// @return Return the distortion RGB camera lens parameters
-	const CamDistortion & getDistortionParameters() override ;
+	const datastructure::CamDistortion & getDistortionParameters() override ;
 
     /// @return Return the distortion depth camera lens parameters
-    const CamDistortion& getDistortionDepthParameters() const override ;
+    const datastructure::CamDistortion& getDistortionDepthParameters() const override ;
 
     void unloadComponent () override final;
 
@@ -156,14 +155,14 @@ private:
 	 * \param color_frame realsense color frame
 	 * \return Solar Image corresponding to given frame
 	 */
-	static SRef<Image> rgbFrameToImage(const rs2::frame& color_frame);
+	static SRef<datastructure::Image> rgbFrameToImage(const rs2::frame& color_frame);
 	/**
 	 * \brief Convert a realsense depth frame to a SolAR image
 	 * \param depth_frame realsense depth frame
 	 * \param colorize set >=0 to use rs2::colorize, color use depends of value
 	 * \return Solar Image corresponding to given frame
 	 */
-	static SRef<Image> depthFrameToImage(const rs2::frame& depth_frame,const int colorize=-1);
+	static SRef<datastructure::Image> depthFrameToImage(const rs2::frame& depth_frame,const int colorize=-1);
 
 	/**
 	 * \brief Update frameset with lastest images
@@ -181,7 +180,7 @@ private:
 	 * \param point output 3dpoint
 	 */
 	void pixelToWorld(const uint16_t* pixels_depth,
-		int w, int h, int i, int j, Point3Df& point) const;
+		int w, int h, int i, int j, datastructure::Point3Df& point) const;
 
 
 	/**
@@ -189,14 +188,14 @@ private:
 	 * \param colorImg Output image
 	 * \return true if success, false otherwise
 	 */
-	bool fillRGBImage(SRef<Image>& colorImg);
+	bool fillRGBImage(SRef<datastructure::Image>& colorImg);
 
 	/**
 	 * \brief Takes last depth image from realsense frameset and put it in depthImg
 	 * \param depthImg Output image
 	 * \return true if success, false otherwise
 	 */
-	bool fillDepthImage(SRef<Image>& depthImg);
+	bool fillDepthImage(SRef<datastructure::Image>& depthImg);
 
 
 	/// Current depth scale
@@ -232,7 +231,7 @@ private:
     rs2_extrinsics m_depth_to_color;
 
 	// SolAR
-	CameraParameters m_parameters;
+	datastructure::CameraParameters m_parameters;
     float m_depth_minimum_distance=0.105f;
 
 	/// call rs2::colorize
@@ -241,10 +240,10 @@ private:
 	/// Contains the calibration and distortion of a stream (rbg or depth)
 	struct CameraInformation
 	{
-		CamCalibration calibration = CamCalibration::Identity();
-		CamDistortion distortion = CamDistortion::Zero();
+		datastructure::CamCalibration calibration = datastructure::CamCalibration::Identity();
+		datastructure::CamDistortion distortion = datastructure::CamDistortion::Zero();
 		/// Default value for stream
-		Sizei size=Sizei();
+		datastructure::Sizei size = datastructure::Sizei();
 		int framerate = 30;
 		/// Extract realsense intrinsics to fill this structure (calibration and distortion)
 		void extractRSIntrinsics(const rs2_intrinsics& intrinsics);
